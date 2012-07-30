03d04f22cd1a3c4c80accb02aa5dfb8dae23ff46
Anatoly Kudinov <zz@rialabs.org>
2012-07-30 17:56:12 +0400

    php

--- init.el
+++ init.el
@@ -1,18 +1,16 @@
-(tool-bar-mode nil)
-(scroll-bar-mode nil)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 (require 'whitespace)
 
 ;; whitespace-mode
 ;; free of trailing whitespace and to use 80-column width, standard indentation
 (setq whitespace-style '(face trailing tabs lines-tail empty space-before-tab
-indentation space-after-tab)
+indentation space-after-tab newline newline-mark space-mark tab-mark)
       whitespace-line-column 80)
 
 (setq ido-enable-flex-matching +1)
-
 (setq ido-everywhere +1)
-
 (setq ido-create-new-buffer 'always)
 
 (ido-mode +1)
@@ -26,7 +24,29 @@ indentation space-after-tab)
 ;; php
 (add-to-list 'load-path "~/.emacs.d/vendor/php-mode")
 (require 'php-mode)
+(setq c-basic-offset 4)
+(setq c-block-comment-prefix "*")
+(add-hook 'php-mode-hook
+	  '(lambda ()
+	     (setq indent-tabs-mode nil)
+	     (whitespace-mode +1)))
 
 ;; coffee
 (add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
 (require 'coffee-mode)
+
+;; yas
+(add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
+(require 'yasnippet)
+(setq yas/snippet-dirs
+      '("~/.emacs.d/snippets/"
+	"~/.emacs.d/vendor/snippets/php-mode/"
+	))
+(yas/reload-all)
+(add-hook 'prog-mode-hook
+          '(lambda ()
+             (yas/minor-mode)))
+
+;; solarized
+(add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/solarized")
+(load-theme 'solarized-dark t)
